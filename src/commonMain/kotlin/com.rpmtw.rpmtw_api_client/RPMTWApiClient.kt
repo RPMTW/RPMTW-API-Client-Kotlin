2a09e3409c6bd7db98aa709e8375f3feec6c8d2c
SiongSng <rrt467778@gmail.com>
2022-02-19 09:26:12 +0800

    added test

--- src/commonMain/kotlin/com.rpmtw.rpmtw_api_client/RPMTWApiClient.kt
+++ src/commonMain/kotlin/com.rpmtw.rpmtw_api_client/RPMTWApiClient.kt
@@ -1,15 +1,12 @@
-@file:JvmName("RPMTWApiClient")
-
 package com.rpmtw.rpmtw_api_client
 
-import kotlin.jvm.JvmName
 import kotlin.jvm.JvmStatic
 
 private var apiClient: RPMTWApiClient? = null
-private var globalToken: String? = null
 
 class RPMTWApiClient(development: Boolean = false, baseUrl: String? = null, token: String? = null) {
     private var apiBaseUrl: String
+    private var globalToken: String? = null
     val baseUrl: String
         get() = apiBaseUrl
 
@@ -17,15 +14,6 @@ class RPMTWApiClient(development: Boolean = false, baseUrl: String? = null, toke
         @JvmStatic
         private val uninitializedException: Exception =
             Exception("Client is not initialized, please call RPMTWApiClient.init() first")
-
-        @JvmStatic
-        fun getInstance(): RPMTWApiClient {
-            if (apiClient == null) {
-                throw uninitializedException
-            } else {
-                return apiClient!!
-            }
-        }
 
         @JvmStatic
         val instance: RPMTWApiClient
@@ -38,20 +26,23 @@ class RPMTWApiClient(development: Boolean = false, baseUrl: String? = null, toke
             }
 
         @JvmStatic
-        fun init(development: Boolean = false, baseUrl: String? = null, token: String? = null) {
-            apiClient = RPMTWApiClient(development, baseUrl)
-            if (token != null) globalToken = token
-        }
-
-        @JvmStatic
-        fun setGlobalToken(token: String) {
-            globalToken = token
+        fun init(development: Boolean = false, baseUrl: String? = null, token: String? = null): RPMTWApiClient {
+            val client = RPMTWApiClient(development, baseUrl, token)
+            apiClient = client
+            return client
         }
     }
 
+    fun setGlobalToken(token: String) {
+        globalToken = token
+    }
+
+    val apiGlobalToken: String?
+        get() = globalToken
+
 
     init {
-        apiBaseUrl = baseUrl ?: if (development) "http://0.0.0.0:8080" else "https://api.rpmtw.com:2096"
+        apiBaseUrl = baseUrl ?: if (development) "http://localhost:8080" else "https://api.rpmtw.com:2096"
         apiClient = this
         if (token != null) globalToken = token
     }
---
+++ src/jvmTest/kotlin/com/rpmtw/rpmtw_api_client/RPMTWApiClientTest.kt
@@ -0,0 +1,45 @@
+package com.rpmtw.rpmtw_api_client
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class RPMTWApiClientTest {
+    @Test
+    fun getBaseUrl() {
+        val client = RPMTWApiClient()
+        assertEquals(client.baseUrl, "https://api.rpmtw.com:2096")
+    }
+
+    @Test
+    fun getBaseUrlWithDevelopment() {
+        val client = RPMTWApiClient(development = true)
+        assertEquals(client.baseUrl, "http://localhost:8080")
+    }
+
+    @Test
+    fun getBaseUrlCustom() {
+        val client = RPMTWApiClient(baseUrl = "http://localhost:1234")
+        assertEquals(client.baseUrl, "http://localhost:1234")
+    }
+
+    @Test
+    fun init() {
+        val client = RPMTWApiClient.init()
+        assertEquals(client, RPMTWApiClient.instance)
+    }
+
+    @Test
+    fun setGlobalToken() {
+        val client = RPMTWApiClient()
+        val token =
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+        client.setGlobalToken(token)
+        assertEquals(client.apiGlobalToken, token)
+    }
+
+    @Test
+    fun getInstance() {
+        val client = RPMTWApiClient()
+        assertEquals(client, RPMTWApiClient.instance)
+    }
+}
